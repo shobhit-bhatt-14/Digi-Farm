cf70fa24f8957b68c10a6be7f804d58def7d7b8d
shobhit-bhatt-14 <shobhit.bhatt.14@gmail.com>
2022-03-31 12:44:02 +0530

    update active link logic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
     def active_link_to name, path
-        link_to name, path, class: "#{ 'active' if current_page? path } nav-link text-white"
+        link_to name, path, class: "#{ 'active' if request.path.split("/")[1] == path.split("/")[1] } nav-link text-white"
     end
 
     def get_user userId
