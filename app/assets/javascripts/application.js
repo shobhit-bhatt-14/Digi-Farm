b87c2111bf80fc59d280cc2c97e75037b3f2fbaf
shobhit-bhatt-14 <shobhit.bhatt.14@gmail.com>
2022-04-01 15:28:34 +0530

    defining view for create post

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,7 @@
 function openCommentForm(ele_id) {
     document.getElementById(ele_id).classList.toggle("hide");
 }
+
+function toggleReactionButton(ele_id) {
+    document.getElementById(ele_id).classList.toggle("active");
+}
